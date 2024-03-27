43c3e6580f7858502da1792a246cb1acde23f779
Sebastian McMillan <sebastian.mcmillan@shu.technology>
2024-03-27 11:42:49 -0500

    Created PlatformIO project

---
+++ src/userinterface.h
@@ -0,0 +1,24 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <SPI.h>
+#include "Adafruit_HX8357.h"
+#include "TouchScreen.h"
+
+// These are the four touchscreen analog pins
+#define YP A2  // must be an analog pin, use "An" notation!
+#define XM A3  // must be an analog pin, use "An" notation!
+#define YM A4  // can be a digital pin
+#define XP A5  // can be a digital pin
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 110
+#define TS_MINY 80
+#define TS_MAXX 900
+#define TS_MAXY 940
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+// The display uses hardware SPI, plus #9 & #10
+#define TFT_RST -1  // dont use a reset pin, tie to arduino RST if you like
+#define TFT_DC 9
+#define TFT_CS 10
